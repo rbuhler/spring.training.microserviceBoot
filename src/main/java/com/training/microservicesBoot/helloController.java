d03bdf0e6d4f1c131accd65f06df4ef5a12f6d23
Buhler <rodrigo.buhler@sap.com>
2017-07-27 13:31:53 -0300

    Step 3 not working properly.

--- src/main/java/com/training/microservicesBoot/helloController.java
+++ src/main/java/com/training/microservicesBoot/helloController.java
@@ -10,10 +10,32 @@ import java.util.Map;
 @Controller
 public class helloController{
 
-    @RequestMapping("/hi/{name}")
-    public @ResponseBody String hiThere(Map model, @PathVariable String name){
+    @RequestMapping("/step3/{name}")
+    public String infoHTML(Map model, @PathVariable String name){
+        String message ="Step 4 - Response HTML with path variable.";
         model.put("name", name);
 
-        return "Hello " + name;
+        return "hello";
+    }
+
+    @RequestMapping("/step2/{name}")
+    public @ResponseBody String hiThere(Map model, @PathVariable String name){
+        String message ="Step 3 - Response body with path variable.";
+        model.put("name", name);
+
+        return message.concat(" -> Hello ").concat(name);
+    }
+
+    @RequestMapping("/step1")
+    public @ResponseBody String map(){
+        String message ="Step 1 - simple URL mapping.";
+
+        return message;
+    }
+    @RequestMapping("/")
+    public @ResponseBody String welcome(){
+        String message ="Step zero - Welcome.";
+
+        return message;
     }
 }
